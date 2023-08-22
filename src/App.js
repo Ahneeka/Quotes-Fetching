1a72ed431bbe6b96404feb949f02397919cba34b
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-08-22 02:07:32 +0100

    save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import { useState } from "react"
 import axios from "axios"
 
 function App() {
   const getQuote = () => {
-    axios.get
+    axios.get('http://api.quotable.io/random')
+    .then(res =>{
+      console.log(res.data.content)
+    }).catch(err =>{
+      console.log(err)
+    })
   }
   return (
     <div className="App">
-     <button onclick={getQuote}>Get Quote</button>
+     <button onClick={getQuote}>Get Quote</button>
     </div>
   );
 }
