fb7b072eab45cf1a08b15fdca01f03155edf0d79
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-08-22 03:25:45 +0100

    save

--- src/Components/Quote.js
+++ src/Components/Quote.js
@@ -1,22 +1,28 @@
-import { useState } from "react"
-import axios from "axios"
+import { useState } from "react";
+import axios from "axios";
+import styles from "./Quote/styles.module.css";
 
 function Quote() {
-  const [quote, setQuote] = useState("")
+  const [quote, setQuote] = useState("");
   const getQuote = () => {
-    axios.get('http://api.quotable.io/random')
-    .then(res =>{
-      console.log(res.data.content)
-      setQuote(res.data.content)
-    }).catch(err =>{
-      console.log(err)
-    })
-  }
+    axios
+      .get("http://api.quotable.io/random")
+      .then((res) => {
+        console.log(res.data.content);
+        setQuote(res.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <div className="App">
-     <button onClick={getQuote}>Get Quote</button>
-     {/* { quote ? <p>{quote}</p> : null }  */}
-     { quote && <p>{quote}</p>}
+    <div className={styles.container}>
+      <div className={styles.con2}>
+        <button className={styles.con3} onClick={getQuote}>Get Quote</button>
+        {/* { quote ? <p>{quote}</p> : null }  */}
+                {/* or */}
+        {quote && <p>{quote}</p>}
+      </div>
     </div>
   );
 }
